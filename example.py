6ec5cd4f33f10a2a5f12ffb31be8f90ed32bd81a
Alligitor <8754766+alligitor@users.noreply.github.com>
2025-04-28 11:50:58 -0400

    added a loop for sending some initial messages

--- example.py
+++ example.py
@@ -1,9 +1,43 @@
 from lxmfbot import LXMFBot
+import socket
 
-bot = LXMFBot("testbot")
+def read_file_to_array(file_path):
+    lines = []  # Initialize an empty list
+    try:
+        with open(file_path, 'r') as file:
+            # Read each line and append it to the list
+            for line in file:
+                lines.append(line.strip())  # Using strip() to remove any leading/trailing whitespace
+        return lines
+    except FileNotFoundError:
+        print(f"Error: The file at {file_path} was not found.")
+        return []
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return []
+
+
+# Main
+hostname = socket.gethostname()
+
+print(hostname)
+
+#bot = LXMFBot("testbot")
+bot = LXMFBot(hostname)
 
 @bot.received
 def echo_msg(msg):
+    print(f"Message {msg}")
     msg.reply(msg.content)
+
+bot._announce()
+
+
+#if there is a file with destinations, send them a message with our host name
+destinations = read_file_to_array("initial_message.txt")
+
+for dest in destinations:
+    print("Sending message to " + dest)
+    bot.send(dest, hostname)
 
 bot.run()
